f44bd4437aeb340d3d99dcebee3d8bf8f5766d54
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2024-01-16 09:56:19 -0300

    Migrando e Componetizando a FEB-P5 para Angular

--- atividades_praticas/FEB-P006/UESC-app/src/app/app.module.ts
+++ atividades_praticas/FEB-P006/UESC-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DestaquesComponent } from './destaques/destaques/destaques.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DestaquesComponent
   ],
   imports: [
     BrowserModule,
